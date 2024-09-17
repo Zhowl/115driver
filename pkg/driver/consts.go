c01539b56b3ab0cd8d4db7df398bb76ded8db342
Zhowl <40994669+Zhowl@users.noreply.github.com>
2024-09-17 22:01:09 +0800

    Update consts.go
    update ua

--- pkg/driver/consts.go
+++ pkg/driver/consts.go
@@ -2,10 +2,10 @@ package driver
 
 const (
 	UADefalut    = "Mozilla/5.0"
-	UA115Browser = "Mozilla/5.0 115Browser/23.9.3.2"
-	UA115Disk    = "Mozilla/5.0 115disk/30.1.0"
-	UA115Desktop = "Mozilla/5.0 115Desktop/2.0.3.6"
-	UAIosApp     = "Mozilla/5.0; Darwin/10.0; UDown/30.1.0"
+	UA115Browser = "Mozilla/5.0 115Browser/27.0.3.7"
+	UA115Disk    = "Mozilla/5.0 115disk/32.7.0"
+	UA115Desktop = "Mozilla/5.0 115Desktop/2.0.10.2"
+	UAIosApp     = "Mozilla/5.0; Darwin/10.0; UDown/32.3.1"
 )
 
 const (
